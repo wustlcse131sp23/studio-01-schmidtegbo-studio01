9a16ac314e75bac7d2e0b089738e62e65e3acb1c
nicoschmidt <nicoschmidt@localhost>
2023-01-26 12:52:00 -0600

    Studio 1!

---
+++ src/studio1/Average.java
@@ -0,0 +1,22 @@
+package studio1;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner in = new Scanner(System.in);
+System.out.println("The first of the two integers to be averaged");
+int n1 = in.nextInt();
+System.out.println("The first of the two integers to be averaged");
+int n2 = in.nextInt();
+
+System.out.print((n1+n2)/2.0);
+
+	
+	
+	}
+
+}
